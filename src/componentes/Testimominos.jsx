e9bf82278701cba78518ab7370e98b831110fe66
maxigiordani <maxigiordani05@gmail.com>
2024-01-04 01:27:59 -0300

    firt commit

---
+++ src/componentes/Testimominos.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Testimonio()  {
+    return (
+<div className="contenedor-testimonio"> 
+<img  className="imagen-testimonio" src={require("../imaganes/avatar-maxi.png")} alt="imagen"/>
+</div>
+    );
+}
